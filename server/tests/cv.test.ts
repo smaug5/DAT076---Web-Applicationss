94e4ee883fc1c1594977549ccaade93b47d8f003
Dunkaburk <jonte8000@hotmail.com>
2024-02-22 14:25:31 +0100

    tests

--- server/tests/cv.test.ts
+++ server/tests/cv.test.ts
@@ -1,12 +1,19 @@
 
-// import { cvService } from '../service/cvServices';
-// import { CV } from '../model/cv';
+import { cvService } from '../src/service/cvServices';
+import { CV } from '../src/model/cv';
 
-// test('should return a cv', async () => {
-//   const cv = await cvService.getCV();
-//   expect(cv).toBeInstanceOf(CV);
-//   expect(cv instanceof CV).toBeTruthy();
-// })
-test("This is a stub2", async () => {
-  expect(1===1).toBeTruthy();
-});
+test('should return a cv', async () => {
+  const cv = await cvService.getCV();
+  expect(isCV(cv)).toBeTruthy();
+})
+
+function isCV(object: any): object is CV {
+  return 'file' in object;
+}
+
+//Check if cv has been added 
+
+test('should add a cv', async () => {
+  const cv = await cvService.addCV("Test"); //Add file here instead
+  expect(cv).toBe("Test");
+})
