f7c60b27a0ca19713d425aeb59cf3bd71a05e306
Dunkaburk <jonte8000@hotmail.com>
2024-02-23 01:35:58 +0100

    Add new CSS files and update HTML and App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
+    <div className="App"> basically min fråga är
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
