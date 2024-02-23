909371acae5545d72419cca52d8d52382666a2ac
Wanda Wannelöf - Smaug <wannelof@student.chalmers.se>
2024-02-23 10:57:54 +0100

    Fixed test versions
    Changed what test package version were being used, should be working now.

--- server/tests/app.test.ts
+++ server/tests/app.test.ts
@@ -1,9 +1,11 @@
-import * as Supertest from "supertest"; // behövs
+import * as SuperTest from "supertest"; // behövs
 import { app } from "../src/start"; // Behövs
 
 // test("Start application", async () => {
     
 // });
+
+const request = SuperTest.default(app);
 
 test("This is a stub", async () => {
     expect(1===1).toBeTruthy();
@@ -15,7 +17,11 @@ test("End-to-end test", async () => {
   const title = "First project";
   const desc = "FIRST!!1!";
 
-  const res1 = await Request.post("/api/project".send(id, title, desc)); // request funkar inte för du har inte definerat request som vi gör på rad 5 i streckis-Wap. Du kommer nog också behöva importsen
+  const res1 = await request.post("/api/project").send({
+    "id": id,
+    "title": title,
+    "desc": desc
+  }); // request funkar inte för du har inte definerat request som vi gör på rad 5 i streckis-Wap. Du kommer nog också behöva importsen
 
 
 });
