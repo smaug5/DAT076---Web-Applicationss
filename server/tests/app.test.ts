5b089c776784d8cbaa540d36d9198480f62dac2f
Wanda Wannelöf - Smaug <wannelof@student.chalmers.se>
2024-02-29 11:24:46 +0100

    End-to-end test added. Updated projectRouter and projectService according to test results.

--- server/tests/app.test.ts
+++ server/tests/app.test.ts
@@ -1,5 +1,7 @@
 import * as SuperTest from "supertest"; // behövs
 import { app } from "../src/start"; // Behövs
+import { project } from "../src/model/project";
+
 
 // test("Start application", async () => {
     
@@ -7,21 +9,35 @@ import { app } from "../src/start"; // Behövs
 
 const request = SuperTest.default(app);
 
-test("This is a stub", async () => {
-    expect(1===1).toBeTruthy();
-  });
-  // TODO make an end-to-end test - Smaug
-
+// Creates two projects and checks that those confirm that they're added. Then looks for one of the projects and checks that the correct one got returned.
 test("End-to-end test", async () => {
   const id = 123;
+  const id2 = 234;
   const title = "First project";
+  const title2 = "Second project";
   const desc = "FIRST!!1!";
+  const desc2 = "SECOND";
 
-  const res1 = await request.post("/api/project").send({
+  const res1 = await request.put("/api/project").send({
     "id": id,
     "title": title,
     "desc": desc
-  }); // request funkar inte för du har inte definerat request som vi gör på rad 5 i streckis-Wap. Du kommer nog också behöva importsen
+  });
+    expect(res1.statusCode).toEqual(200);
 
+  const res1_2 = await request.put("/api/project").send({
+    "id" : id2,
+    "title" : title2,
+    "desc" : desc2
+  })
+   expect(res1_2.statusCode).toEqual(200);
+   
+   const res2 = await request.get("/api/project");
+   expect(res2.statusCode).toEqual(200);
+   expect(res2.body.map((project : project) => project.title)).toContain(title);
+   expect(res2.body.map((project : project) => project.title)).toContain(title2);
+
+  const res3 = await request.get("/api/project/"+title2);
+  expect(res3.body.title).toContain(title2);
 
 });
