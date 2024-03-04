047f182cc6f1b7d09090de5d8cafa4c02e3c34ea
Dunkaburk <jonte8000@hotmail.com>
2024-03-04 14:47:52 +0100

    Remove admin login route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -37,8 +37,6 @@ function App() {
 
             <Route path='projects' element={< ProjectContent /> } />
 
-            <Route path='adminlogin' element={< Login /> } />
-
           </Route>
 
         </Routes>
