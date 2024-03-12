f296760b5169862d57a18d4300e93575accb6601
Dunkaburk <jonte8000@hotmail.com>
2024-03-13 00:39:46 +0100

    Removed outdated files

--- server/db/conn.ts
+++
@@ -1,5 +0,0 @@
-import { createConnection } from "mongoose";
-
-const uri = "mongodb+srv://portfoliowap:<HackerCatNos>@portfolio.zyejove.mongodb.net/?retryWrites=true&w=majority&appName=Portfolio"
-
-export const conn = createConnection(uri);
